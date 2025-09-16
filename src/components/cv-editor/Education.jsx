a94218fd2e85704bcdfa3a0c60d27a6e6d049758
Joshua King <162310653+jjjmdev@users.noreply.github.com>
2025-09-17 02:41:50 +0800

    Update(Education): add createEducation and handleEducationChange. Add(Preview): Education section

--- src/components/cv-editor/Education.jsx
+++ src/components/cv-editor/Education.jsx
@@ -1,9 +1,116 @@
+import { v4 as uuidv4 } from 'uuid'
 import Section from './Section'
+import { kebabToCamel } from '../utils'
 
 export default function Education({ data, setData }) {
+  const { education } = data
+
+  const createEducationEntry = () => {
+    setData({
+      ...data,
+      education: [
+        ...data.education,
+        {
+          id: uuidv4(),
+          schoolName: '',
+          degree: '',
+          startDate: '',
+          endDate: '',
+          location: '',
+        },
+      ],
+    })
+  }
+
+  const handleEducationChange = (id, e) => {
+    const { name, value } = e.target
+    const newData = data
+    const selectedEntry = newData.education.find((entry) => entry.id === id)
+
+    selectedEntry[kebabToCamel(name)] = value
+
+    setData({ ...newData })
+  }
+
   return (
     <>
-      <Section title='Education'></Section>
+      <Section title='Education'>
+        {education.map(({ id }) => {
+          const formData = education.find((form) => form.id === id)
+          return (
+            <EducationForm
+              key={id}
+              formData={formData}
+              handleChange={handleEducationChange}
+            />
+          )
+        })}
+        <button className='education-add-button' onClick={createEducationEntry}>
+          Add Education
+        </button>
+      </Section>
+    </>
+  )
+}
+
+function EducationForm({ formData, handleChange }) {
+  console.log(formData)
+  return (
+    <>
+      <div className='education-form'>
+        <div>
+          <label htmlFor='school-name'>School</label>
+          <input
+            type='text'
+            name='school-name'
+            id='school-name'
+            value={formData.schoolName}
+            onChange={(e) => handleChange(formData.id, e)}
+          />
+        </div>
+        <div>
+          <label htmlFor='degree'>Degree</label>
+          <input
+            type='text'
+            name='degree'
+            id='degree'
+            value={formData.degree}
+            onChange={(e) => handleChange(formData.id, e)}
+          />
+        </div>
+        <div className='school-dates'>
+          <div>
+            <label htmlFor='start-date'>Start Date</label>
+            <input
+              type='text'
+              name='start-date'
+              id='start-date'
+              value={formData.startDate}
+              onChange={(e) => handleChange(formData.id, e)}
+            />
+          </div>
+          <div>
+            <label htmlFor='end-date'>End Date</label>
+            <input
+              type='text'
+              name='end-date'
+              id='end-date'
+              value={formData.endDate}
+              onChange={(e) => handleChange(formData.id, e)}
+            />
+          </div>
+        </div>
+        <div>
+          <label htmlFor='location'>Location</label>
+          <input
+            type='text'
+            name='location'
+            id='location'
+            value={formData.location}
+            onChange={(e) => handleChange(formData.id, e)}
+          />
+        </div>
+      </div>
     </>
   )
 }
--- src/components/preview/Preview.jsx
+++ src/components/preview/Preview.jsx
@@ -1,10 +1,10 @@
 export default function Preview({ data }) {
-  const { personalInfo } = data
+  const { personalInfo, education } = data
 
   return (
     <div className='preview'>
       Preview
-      <section className='personal-info-preview'>
+      <section className='preview-personal-info'>
         <h1>
           {personalInfo.firstName} {personalInfo.lastName}
         </h1>
@@ -12,6 +12,23 @@ export default function Preview({ data }) {
           {data.personalInfo.email} {data.personalInfo.phoneNumber}{' '}
           {data.personalInfo.location}
         </p>
+      </section>
+      <section className='preview-education'>
+        <h2>Education</h2>
+        {education.map(
+          ({ schoolName, startDate, endDate, degree, location, id }) => (
+            <div className='preview-school' key={id}>
+              <div>
+                <h3>{schoolName}</h3>
+                <p>
+                  {startDate} – {endDate}
+                </p>
+              </div>
+              <p>{degree}</p>
+              <p>{location}</p>
+            </div>
+          )
+        )}
       </section>
     </div>
   )
