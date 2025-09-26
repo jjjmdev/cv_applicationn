87e8f1935876dda06ad7b3f50817111a4393312c
Joshua King <162310653+jjjmdev@users.noreply.github.com>
2025-09-26 12:23:10 +0800

    Refactor(Preview): Slight refactor for better structure

--- src/components/preview/Preview.jsx
+++ src/components/preview/Preview.jsx
@@ -1,63 +1,72 @@
+import '../styles/preview.css'
+
 export default function Preview({ data }) {
   const { personalInfo, education, experience } = data
 
   return (
     <div className='preview'>
-      Preview
-      <section className='preview-personal-info'>
-        <h1>
-          {personalInfo.firstName} {personalInfo.lastName}
-        </h1>
-        <p>
-          {data.personalInfo.email} {data.personalInfo.phoneNumber}{' '}
-          {data.personalInfo.location}
-        </p>
-      </section>
-      <section className='preview-education'>
-        <h2>Education</h2>
-        {Object.entries(education).map(
-          ([key, { schoolName, startDate, endDate, degree, location }]) => (
-            <div className='preview-school' key={key}>
-              <div>
-                <h3>{schoolName}</h3>
+      <div className='a4-preview-page'>
+        <section className='preview-personal-info'>
+          <h1>
+            {personalInfo.firstName} {personalInfo.lastName}
+          </h1>
+          <p>
+            {data.personalInfo.email} | {data.personalInfo.phoneNumber} |{' '}
+            {data.personalInfo.location}
+          </p>
+        </section>
+        <section className='preview-education'>
+          <h2>Education</h2>
+          <hr />
+          {Object.entries(education).map(
+            ([key, { schoolName, startDate, endDate, degree, location }]) => (
+              <div className='preview-school' key={key}>
+                <div>
+                  <h3>{schoolName}</h3>
+                  <p>
+                    {startDate} – {endDate}
+                  </p>
+                </div>
                 <p>
-                  {startDate} – {endDate}
+                  <i>{degree}</i>
                 </p>
+                <p>{location}</p>
               </div>
-              <p>{degree}</p>
-              <p>{location}</p>
-            </div>
-          )
-        )}
-      </section>
-      <section className='preview-experience'>
-        <h2>Experience</h2>
-        {Object.entries(experience).map(
-          ([
-            key,
-            {
-              companyName,
-              jobTitle,
-              startDate,
-              endDate,
-              location,
-              description,
-            },
-          ]) => (
-            <div className='preview-experience' key={key}>
-              <div>
-                <h3>{companyName}</h3>
+            )
+          )}
+        </section>
+        <section className='preview-experience'>
+          <h2>Experience</h2>
+          <hr />
+          {Object.entries(experience).map(
+            ([
+              key,
+              {
+                companyName,
+                jobTitle,
+                startDate,
+                endDate,
+                location,
+                description,
+              },
+            ]) => (
+              <div className='preview-job' key={key}>
+                <div>
+                  <h3>{companyName}</h3>
+                  <p>
+                    {startDate} – {endDate}
+                  </p>
+                </div>
                 <p>
-                  {startDate} – {endDate}
+                  <i>{jobTitle}</i>
                 </p>
+                <p>{location}</p>
+                <p>{description}</p>
               </div>
-              <p>{jobTitle}</p>
-              <p>{location}</p>
-              <p>{description}</p>
-            </div>
-          )
-        )}
-      </section>
+            )
+          )}
+        </section>
+      </div>
     </div>
   )
 }
